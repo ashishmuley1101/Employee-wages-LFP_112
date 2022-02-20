a27339ecd4281753d7c4ee71ba9837974b72e412
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-02-20 19:51:33 +0530

    Claculating the Employee Wages

--- src/com/bridgelab/pratice/IsPresentOrAbsent.java
+++ src/com/bridgelab/pratice/IsPresentOrAbsent.java
@@ -4,15 +4,24 @@ public class IsPresentOrAbsent {
 	
 	public static void main(String[] args) {
 		final int PRESENT = 1;
-		final int DIV = 2;
+		final int PRESENT_NUMBER_RANGE = 2;
+		final int WORKING_HOURS = 8;
+		final int WAGE_PER_HOUR = 30;
 		
-		int isPresent = (int) Math.floor(Math.random()* DIV);
-		System.out.println(isPresent);
+		int isPresent = (int) Math.floor(Math.random()* PRESENT_NUMBER_RANGE);
+		// System.out.println(isPresent);
+		int empWage;
 		if(isPresent==PRESENT){
+			
 			System.out.println("Employee is Present");
+			 empWage = WORKING_HOURS * WAGE_PER_HOUR;
+			
 		} else {
 			System.out.println("Employee is Absent");
+			empWage = 0;
+			
 		}
+		System.out.println("Employee Wage :$ " +empWage+ " USD");
 	}
 
 }
