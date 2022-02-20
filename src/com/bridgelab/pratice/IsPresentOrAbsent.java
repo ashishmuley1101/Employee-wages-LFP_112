c2c6535d89c74d662dcb2ed36c7b3eaefe65787f
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-02-20 21:23:29 +0530

    Part Time Wages Calculation

--- src/com/bridgelab/pratice/IsPresentOrAbsent.java
+++ src/com/bridgelab/pratice/IsPresentOrAbsent.java
@@ -4,7 +4,8 @@ public class IsPresentOrAbsent {
 	
 	public static void main(String[] args) {
 		final int PRESENT = 1;
-		final int PRESENT_NUMBER_RANGE = 2;
+		final int PART_TIME = 2;
+		final int PRESENT_NUMBER_RANGE = 3;
 		final int WORKING_HOURS = 8;
 		final int WAGE_PER_HOUR = 30;
 		
@@ -13,14 +14,23 @@ public class IsPresentOrAbsent {
 		int empWage;
 		if(isPresent==PRESENT){
 			
-			System.out.println("Employee is Present");
+			System.out.println("Employee Working for Full Time");
 			 empWage = WORKING_HOURS * WAGE_PER_HOUR;
 			
+		} else if(isPresent == PART_TIME ) {
+			
+			System.out.println("Employee Working for Part Time");
+			empWage = WORKING_HOURS * (WAGE_PER_HOUR/2);
+			
+			
 		} else {
+			
 			System.out.println("Employee is Absent");
 			empWage = 0;
-			
+
 		}
+		
+	
 		System.out.println("Employee Wage :$ " +empWage+ " USD");
 	}
 
