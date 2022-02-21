83802f0041a3aa253dca80a587ab25cd2092090b
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-02-21 23:51:55 +0530

    Calculating wages for Month

--- src/com/bridgelab/pratice/IsPresentOrAbsent.java
+++ src/com/bridgelab/pratice/IsPresentOrAbsent.java
@@ -8,28 +8,40 @@ public class IsPresentOrAbsent {
 		final int PRESENT_NUMBER_RANGE = 3;
 		final int WORKING_HOURS = 8;
 		final int WAGE_PER_HOUR = 30;
+		final int WORKING_MAX_DAY = 20;
+		double totalWorkingHour = 0;
 		
-		int isPresent = (int) Math.floor(Math.random()* PRESENT_NUMBER_RANGE);
-		// System.out.println(isPresent);
-		int empWage;
-		
-		switch (isPresent) {
-		case PRESENT :
-			System.out.println("Employee Working for Full Time");
-			 empWage = WORKING_HOURS * WAGE_PER_HOUR;
-			break;
+		for(int day = 0 ; day < WORKING_MAX_DAY ; day++ ) {
 			
-		case PART_TIME :
-			System.out.println("Employee Working for Part Time");
-			empWage = WORKING_HOURS * (WAGE_PER_HOUR/2);
-			break;
-			
-		default :
-			System.out.println("Employee is Absent");
-			empWage = 0;
-		}
 		
-		System.out.println("Employee Wage :$ " +empWage+ " USD");
-	}
+			int isPresent = (int) Math.floor(Math.random()* PRESENT_NUMBER_RANGE);
+			
+			int empWage;
+			
+			switch (isPresent) {
+			case PRESENT :
+				totalWorkingHour = totalWorkingHour + WORKING_HOURS;
+			//	System.out.println("Employee Working for Full Time");
+			//	 empWage = WORKING_HOURS * WAGE_PER_HOUR;
+				break;
+				
+			case PART_TIME :
+				totalWorkingHour = totalWorkingHour + (WORKING_HOURS/2);
 
-}
+				//System.out.println("Employee Working for Part Time");
+				//empWage = WORKING_HOURS * (WAGE_PER_HOUR/2);
+				break;
+				
+			default :
+				//System.out.println("Employee is Absent");
+				empWage = 0;
+				break;
+			}
+		
+			//System.out.println("Day : "+day+ " Employee Wage :$ " +empWage+ " USD");
+		}
+		double salary = totalWorkingHour * WAGE_PER_HOUR;
+		System.out.println("Employee Monthly Wage : $" +salary+ " USD (Total Working Hours : " + totalWorkingHour + ")"  );
+		
+	}	
+}	
